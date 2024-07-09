573fde2d8a5794dff02b01d93ac740731d19334c
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2024-07-09 01:16:55 +0100

    bug fix

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ createConnection({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  ssl: sslStatus,
+  ssl: true,
 
   synchronize: true,
   logging: false,
